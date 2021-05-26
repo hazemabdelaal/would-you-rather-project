ccc59ac8165bb68f0c3de080d3325237cac2da15
hazemsamyy <hazemsamyy@gmail.com>
2021-05-27 00:45:59 +0200

    feat: Created redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 import App from './components/App';
 import './index.css';
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
