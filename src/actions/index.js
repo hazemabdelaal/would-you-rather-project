6a57dc97893c6b5b66acaf53736533f5fa44321c
hazemsamyy <hazemsamyy@gmail.com>
2021-05-29 01:37:38 +0200

    feat: Added loadingbar

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import { getQuestions, saveQuestion, saveQuestionAnswer } from './questions';
 import { getUsers, saveUserQuestion, saveUserQuestionAnswer } from './users';
 import { getInitialData } from '../utils/apis';
 import { _saveQuestion, _saveQuestionAnswer } from '../utils/_DATA';
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 export const handleInitialData = () => dispatch =>
   getInitialData().then(({ users, questions }) => {
@@ -10,17 +11,21 @@ export const handleInitialData = () => dispatch =>
   });
 
 export const handleSaveQuestion = question => (dispatch, getState) => {
+  dispatch(showLoading());
   const { authedUser } = getState();
 
   return _saveQuestion(question).then(ques => {
     dispatch(saveQuestion(ques));
     dispatch(saveUserQuestion(authedUser, ques.id));
+    dispatch(hideLoading());
   });
 };
 
 export const handleAnswerQuestion = info => dispatch => {
+  dispatch(showLoading());
   return _saveQuestionAnswer(info).then(() => {
     dispatch(saveQuestionAnswer(info));
     dispatch(saveUserQuestionAnswer(info));
+    dispatch(hideLoading());
   });
 };
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { handleInitialData } from '../actions/index';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoadingBar, { showLoading, hideLoading } from 'react-redux-loading-bar';
 import NavBar from './NavBar';
 import Login from './Login';
 import Home from './Home';
@@ -14,13 +15,15 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(handleInitialData());
+    dispatch(showLoading());
+    dispatch(handleInitialData()).then(() => dispatch(hideLoading()));
   }, [dispatch]);
 
   const authedUser = useSelector(state => state.authedUser);
 
   return (
     <Router>
+      <LoadingBar />
       <NavBar />
       {authedUser === null ? (
         <Route component={Login} />
