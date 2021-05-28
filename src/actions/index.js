63d73ce2f7c62d79411c18dc8c57be105409eebd
hazemsamyy <hazemsamyy@gmail.com>
2021-05-29 01:26:44 +0200

    feat: Added saving answers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import { getQuestions, saveQuestion, saveQuestionAnswer } from './questions';
-import { getUsers } from './users';
+import { getUsers, saveUserQuestion, saveUserQuestionAnswer } from './users';
 import { getInitialData } from '../utils/apis';
 import { _saveQuestion, _saveQuestionAnswer } from '../utils/_DATA';
 
@@ -9,13 +9,18 @@ export const handleInitialData = () => dispatch =>
     dispatch(getQuestions(questions));
   });
 
-export const handleSaveQuestion = question => dispatch =>
-  _saveQuestion(question).then(ques => {
+export const handleSaveQuestion = question => (dispatch, getState) => {
+  const { authedUser } = getState();
+
+  return _saveQuestion(question).then(ques => {
     dispatch(saveQuestion(ques));
+    dispatch(saveUserQuestion(authedUser, ques.id));
   });
+};
 
 export const handleAnswerQuestion = info => dispatch => {
   return _saveQuestionAnswer(info).then(() => {
     dispatch(saveQuestionAnswer(info));
+    dispatch(saveUserQuestionAnswer(info));
   });
 };
--- src/actions/users.js
+++ src/actions/users.js
@@ -4,3 +4,16 @@ export const getUsers = users => ({
   type: actions.GET_USERS,
   users,
 });
+
+export const saveUserQuestion = (authedUser, qid) => ({
+  type: actions.SAVE_USER_QUESTION,
+  authedUser,
+  qid,
+});
+
+export const saveUserQuestionAnswer = (authedUser, qid, option) => ({
+  type: actions.SAVE_USER_QUESTION_ANSWER,
+  authedUser,
+  qid,
+  option,
+});
